8c82a56d9321f680cd3ce5fedc0974993f53c7a8
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-10-18 16:31:41 -0300

    Se agregan etiquetas a los integrantes de los proyectos

--- hello.js
+++ hello.js
@@ -49,14 +49,12 @@ app.use('/lab/intern',intern);
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
-    console.log("?");
     next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
-    console.log(err);
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     // render the error page
