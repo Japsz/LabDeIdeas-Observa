7d33bd23cfda54232d36ebc96ac20db09f27ea6a
benjamin meneses <benjamin.meneses.14@sansano.usm.cl>
2019-06-29 20:00:08 -0400

    pd commit anterior
    faltó un archivo y una contraseña no correcta

--- routes/intern.js
+++ routes/intern.js
@@ -9,7 +9,7 @@ router.use(
 
         host: '127.0.0.1',
         user: 'root',
-        password : 'observaproyecta',
+        password : '1234',
         port : 3306,
         database:'Observapp',
         insecureAuth : true
